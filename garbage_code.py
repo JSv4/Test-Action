6f31e40978151c71b5752bc6f9fa18ec3c161f55
John Scrudato <jscrudato@gunder.com>
2022-05-17 09:57:41 -0400

    Updated tests.

--- garbage_code.py
+++ garbage_code.py
@@ -1,5 +1,10 @@
 def do_some_stuff():
     return True
 
+
+def i_am_five():
+    return 5
+
+
 if __name__ == '__main__':
     print('I am a waste of CPU time.')
--- tests/stupid_test.py
+++ tests/stupid_test.py
@@ -1,5 +1,8 @@
-from garbage_code import do_some_stuff
+from garbage_code import do_some_stuff, i_am_five
 
 def test_stupid_python_code():
 	assert(True is True)
 	assert(do_some_stuff() is True)
+
+def test_another_dumb_thing():
+	assert(i_am_five() == 5)
